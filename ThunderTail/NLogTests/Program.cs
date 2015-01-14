5595d99cf0b4d2971813d0544d5390076afbef2d
Xiaowei <thundertail@gmail.com>
2015-01-14 23:23:50 +0000

    Nlog Test

---
+++ ThunderTail/NLogTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Fluent;
+
+namespace NLogTests
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Logger.Info("{0}", DateTime.Now);
+            }
+        }
+    }
+}
