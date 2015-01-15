e4326660694493047203fab1b924e759251e9219
RRHHAAWW <andywangrh@hotmail.com>
2015-01-14 23:17:12 -0500

    Changed scripts to external
    将TimeZone script从Intex.cshtml中移除，改为external。

--- ThunderTail/ContactManager/Global.asax.cs
+++ ThunderTail/ContactManager/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace ContactManager
 {
@@ -17,5 +18,6 @@ namespace ContactManager
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        
     }
 }
---
+++ ThunderTail/ContactManager/Timezone.js
@@ -0,0 +1,24 @@
+﻿var LocalTime_hr;
+var LocalTime_mn;
+var LocalTime;
+
+function testFunction(a, b) {
+    var c = a + b;
+    document.getElementById("scriptOutput").innerHTML = c;
+}
+function resetFunction() {
+    document.getElementById("scriptOutput").innerHTML = "Here shows the sum of 4 and 5.";
+}
+function Time() {
+    LocalTime_hr = document.getElementById("Hour").value;
+    LocalTime_mn = document.getElementById("Minute").value;
+    if (LocalTime_hr > 24 || LocalTime_hr < 0 || LocalTime_mn > 60 || LocalTime_mn < 0) {
+        alert("Invalid time format!");
+    } else {
+        LocalTime_hr -= 5;
+    }
+    if (LocalTime_mn < 10) {
+        LocalTime_mn = '0' + LocalTime_mn;
+    }
+    document.getElementById("Ottawa").innerHTML = LocalTime_hr + ':' + LocalTime_mn;
+}
