1a2f583185ed5d50f25e0d7f3860ecf6ad1c36ab
RRHHAAWW <andywangrh@hotmail.com>
2015-01-17 16:42:28 -0500

    Added table and search box

--- ThunderTail/ContactManager/Scripts/Timezone.js
+++ ThunderTail/ContactManager/Scripts/Timezone.js
@@ -5,6 +5,7 @@ var LocalTime;
 function testFunction(a, b) {
     var c = a + b;
     document.getElementById("scriptOutput").innerHTML = c;
+    JL().info("Run pressed.");
 }
 function resetFunction() {
     document.getElementById("scriptOutput").innerHTML = "Here shows the sum of 4 and 5.";
